f4a7f95224b4b372bdfb889aad1371cf1e13b4b2
sterling.walsh.dev@gmail.com <sterling.walsh.dev@gmail.com>
2019-05-06 12:05:17 -0400

    remove default image

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -11,7 +11,7 @@ class ImageLinkForm extends Component{
                 </p>
                 <div className='center'>
                     <div className='pa4 br3 shadow-5 form center'>
-                        <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} defaultValue='https://previews.123rf.com/images/heckmannoleg/heckmannoleg1604/heckmannoleg160400021/54978500-beautiful-woman-face-perfect-makeup-beauty-fashion-eyelashes-cosmetic-eyeshadow-highlighting.jpg'/>
+                        <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
                         <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
                                 onClick={onImageSubmit}>
                             Detect
